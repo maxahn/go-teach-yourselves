b9566e4232b896f2fdb0f10f087da5074438bdd8
maxahn <ahnmaximus@gmail.com>
2020-10-16 23:02:46 -0600

    created starting pages and links that navigates to the pages

---
+++ src/components/App/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, 
+  Route,
+ } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+    </div>
+  </Router>
+);
+
+export default App;
