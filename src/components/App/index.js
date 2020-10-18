78e0a08fddd0f4c86d5956b42ce1e0b4df70beb5
maxahn <ahnmaximus@gmail.com>
2020-10-17 21:09:52 -0600

    implemented higher order components to remove the session functionality out of th app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,10 @@ import {
   BrowserRouter as Router, 
   Route,
  } from 'react-router-dom';
+// import { withAuthentication } from '../Session';
 import { withFirebase } from '../Firebase';
+import { AuthUserContext } from '../Session';
+import { withAuthentication } from '../Session';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -14,45 +17,22 @@ import AccountPage from '../Account';
 
 import * as ROUTES from '../../constants/routes';
 
-class App extends Component { 
-  constructor(props) {
-    super(props);
-    this.state = {
-      authUser: null
-    };
-  };
+const App = (props) => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      {
+        props.authUser && (<div>
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        </div>)
+      }
+    </div>
+  </Router>
+);
 
-  componentDidMount() {
-    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-      let newAuthUser = authUser ? authUser : null; 
-      this.setState({authUser: newAuthUser});
-    });
-  }
-
-  componentWillUnmount() {
-    this.listener();
-  }
-  
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser}/>
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-          {
-            this.state.authUser && (
-              <div>
-                <Route exact path={ROUTES.HOME} component={HomePage} />
-                <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-              </div>
-            )
-          }
-        </div>
-      </Router>
-    );
-  };
-};
-
-export default withFirebase(App);
+export default withAuthentication(App);
